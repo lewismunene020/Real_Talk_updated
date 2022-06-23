dad9f8ae4bc518bb33d2f8454c780233839fe8a2
lewismunene020 <98200048+lewismunene020@users.noreply.github.com>
2022-06-23 17:54:13 +0300

    Update config.php

--- php_files/config.php
+++ php_files/config.php
@@ -1,6 +1,6 @@
 <?php
    
-    $conn = mysqli_connect("sql4.freesqldatabase.com:3306" ,"sql4499851" ,"iRjSLcHdWB" , "sql4499851");
+    $conn = mysqli_connect("sql11.freesqldatabase.com:3306" ,"sql11501468" ,"LjuNgqIR8p" , "sql4499851");
     if(!$conn){
        echo "something went wrong";
     }
@@ -10,7 +10,7 @@
 
     //    inserting into data  
     function insert_data($name){
-        $conn = mysqli_connect("sql4.freesqldatabase.com:3306" ,"sql4499851" ,"iRjSLcHdWB" , "sql4499851");
+        $conn = mysqli_connect("sql4.freesqldatabase.com:3306" ,"sql11501468" ,"LjuNgqIR8p" , "sql11501468");
         $sql = "INSERT INTO `firebase-upload` (`image-name`) VALUES ('{$name}');";
         $result = mysqli_query($conn ,$sql);
         if($result){
