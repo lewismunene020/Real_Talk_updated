274f607badfe17655f5a290ff454ef07d3e74bf9
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-11 12:17:35 +0300

    setting up  github manager to  pull  request  from  githubwebhook

--- automate/github__manager.php
+++ automate/github__manager.php
@@ -2,6 +2,10 @@
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-echo $data
+foreach ($data as $data2 => $value) {
+    echo "{ ".$data2." : ".$value." } \n\n";
+}
 
+$pull  = exec("./pullGit");
+echo $pull;
 ?>
