bfdfb915457c02b1e117d4175727f35be71f70af
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-11 15:15:55 +0300

    setting  up   the   python github pulling script

--- automate/github__manager.py
+++ automate/github__manager.py
@@ -5,12 +5,12 @@ from automate import color__manager as cm
 cm.printSuccess("Someone commited to your appprobably  you")
 
 
-# os.system( 'eval "$(ssh-agent -s)"')
-# os.system("ssh-add ~/.ssh/githubssh")
-# command =  'git  pull  origin master'
-# os.system(command)
+os.system( 'eval "$(ssh-agent -s)"')
+os.system("ssh-add ~/.ssh/githubssh")
+command =  'git  pull  origin master'
+os.system(command)
 # print(cmd.processData(command))
 # output  = cmd.processData(command)
-os.system('./pullGit')
+# os.system('./pullGit')
 
 cm.printSuccess("Someone commited to your appprobably  you")
