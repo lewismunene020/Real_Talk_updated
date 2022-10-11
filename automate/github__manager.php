44ee38788b2181e109a41aca2e3e2f510b9fa338
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-11 14:26:52 +0300

    setting up  guhub  pulling  script

--- automate/github__manager.php
+++ automate/github__manager.php
@@ -2,21 +2,21 @@
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-foreach ($data as $data2 => $value) {
-    echo "{ ".$data2." : ".$value." } \n\n";
-}
+// foreach ($data as $data2 => $value) {
+//     echo "{ ".$data2." : ".$value." } \n\n";
+// }
 
-$pull  = exec(' eval "$(ssh-agent -s)" && ' );
-echo $pull;
-$pull  = exec('ssh-add  ~/.ssh/githubssh &&');
-echo $pull;
-$pull  = exec('git pull  origin master &&');
-echo $pull;
+$pull  = exec('eval "$(ssh-agent -s)"');
+// echo $pull;
+$pull  = exec('ssh-add  ~/.ssh/githubssh');
+// echo $pull;
+$pull  = exec('git pull  origin master');
+// echo $pull;
 
-$pull  = exec('GREEN="\033[0;32m" && echo"$GREEN CHANGES PULLED SUCCESSFULLY "');
-echo $pull;
+// $pull  = exec('GREEN="\033[0;32m" && echo"$GREEN CHANGES PULLED SUCCESSFULLY "');
+// echo $pull;
 
-$reply  = shell_exec("./pullGit")
-
+$reply  = shell_exec("./pullGit");
+echo $reply;
 
 ?>
