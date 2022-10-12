9dc5ab709b3a7618d99d3c13b55bd531627102fb
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-12 12:06:20 +0300

    updating git pull script

--- automate/github__manager.php
+++ automate/github__manager.php
@@ -3,19 +3,21 @@
 $data = json_decode(file_get_contents('php://input'), true);
 
 // echo exec("bash phpExec");
-echo exec('bash pullGit');
+echo "<pre>";
+echo shell_exec('bash pullGit');
+echo "</pre>";
 // foreach ($data as $data2 => $value) {
 //     echo "{ ".$data2." : ".$value." } \n\n";
 // }
 
-// $pull  = exec('eval "$(ssh-agent -s)"');
+// $pull  = shell_exec('eval "$(ssh-agent -s)"');
 // echo $pull;
-// $pull  = exec('ssh-add  ~/.ssh/githubssh');
+// $pull  = shell_exec('ssh-add  ~/.ssh/githubssh');
 // echo $pull;
-// $pull  = exec('git pull  origin master');
+// $pull  = shell_exec('git pull  origin master');
 // echo $pull;
 
-// $pull  = exec('GREEN="\033[0;32m" && echo"$GREEN CHANGES PULLED SUCCESSFULLY "');
+// $pull  = shell_exec('GREEN="\033[0;32m" && echo"$GREEN CHANGES PULLED SUCCESSFULLY "');
 // echo $pull;
 
 
