a8c44b33ba49f81f31a8233cbaa2247935612dc2
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-12 10:50:49 +0300

    still workign on the github aotomation

--- automate/github__manager.php
+++ automate/github__manager.php
@@ -2,6 +2,8 @@
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+// echo exec("bash phpExec");
+echo exec('bash pullGit');
 // foreach ($data as $data2 => $value) {
 //     echo "{ ".$data2." : ".$value." } \n\n";
 // }
@@ -16,7 +18,24 @@ $data = json_decode(file_get_contents('php://input'), true);
 // $pull  = exec('GREEN="\033[0;32m" && echo"$GREEN CHANGES PULLED SUCCESSFULLY "');
 // echo $pull;
 
-echo shell_exec("python3 github__manager.py");
-echo $reply;
+
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    print("<pre>");
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+    print("</pre>");
+}
+// Print the exec output inside of a pre element
+// $command='eval$"(ssh-agent -s)"&ssh-add ~/.ssh/githubssh&&git pull origin master';
+// execPrint($command)
+// execPrint("python3 github__manager.py");
+// execPrint("git status");
+
+
+// echo shell_exec("python3 github__manager.py");
+// echo $reply;
 
 ?>
--- automate/github__manager.py
+++ automate/github__manager.py
@@ -2,15 +2,15 @@
 import  os
 from utils import color__manager as cm
 
-cm.printSuccess("Someone commited to your appprobably  you")
+# cm.printSuccess("Someone commited to your appprobably  you")
 
 
-os.system( 'eval "$(ssh-agent -s)"')
-os.system("ssh-add ~/.ssh/githubssh")
-command =  'git  pull  origin master'
-os.system(command)
+# os.system( 'eval "$(ssh-agent -s)"')
+# os.system("ssh-add ~/.ssh/githubssh")
+# command =  'git  pull  origin master'
+# os.system(command)
 # print(cmd.processData(command))
 # output  = cmd.processData(command)
-# os.system('./pullGit')
+os.system('bash pullGit')
 
-cm.printSuccess("Someone commited to your appprobably  you")
+# cm.printSuccess("Someone commited to your appprobably  you")
